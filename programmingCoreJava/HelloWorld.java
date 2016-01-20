4d93dbd132e36185295e167ded4453ac6f688818
JMatharu <jagdeepmatharu88@gmail.com>
2016-01-19 21:02:59 -0500

    Hello World

---
+++ programmingCoreJava/HelloWorld.java
@@ -0,0 +1,15 @@
+package programmingCoreJava;
+
+/**
+ * 
+ * @author jmatharu Jagdeep Matharu
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
