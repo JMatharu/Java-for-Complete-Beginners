235486ae003f766f6f8bf796426698f708ad8de2
JMatharu <jagdeepmatharu88@gmail.com>
2016-01-24 15:18:55 -0500

    Variables

---
+++ programmingCoreJava/Variable.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package programmingCoreJava;
+
+/**
+ * @author jmatharu Jagdeep Matharu
+ *
+ */
+public class Variable {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myNumber = 100;
+		short myShort = 123;
+		long myLong = 1234;
+		double myDouble = 5.753;
+		float myFloat = 71.21f;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+	}
+
+}
