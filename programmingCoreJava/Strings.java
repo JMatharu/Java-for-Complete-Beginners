c5be4f01aea9755fa25084defb1d111468942d76
JMatharu <jagdeepmatharu88@gmail.com>
2016-02-09 22:07:05 -0500

    Strings

---
+++ programmingCoreJava/Strings.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package programmingCoreJava;
+
+/**
+ * @author jmatharu
+ *
+ */
+public class Strings {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String text = "Hello";
+		String blank = " ";
+		String name = "Bob";
+		String greetings = text + blank + name;
+
+		System.out.println(greetings);
+
+		int myInt = 7;
+		System.out.println("My Integer is : " + myInt);
+	}
+
+}
