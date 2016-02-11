131d4220b8b79dee68bbe463fbea18ee6df33a44
JMatharu <jagdeepmatharu88@gmail.com>
2016-02-10 20:10:14 -0500

    While loops

---
+++ programmingCoreJava/WhileLoops.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package programmingCoreJava;
+
+/**
+ * @author jmatharu
+ *
+ */
+public class WhileLoops {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value = 0;
+
+		//while loop with condition
+		while (value < 10) {
+			System.out.println("Hello!");
+			value++;
+		}
+		
+		//while loop with enumeration
+		
+		//while loop with iterator
+		
+	}
+
+}
