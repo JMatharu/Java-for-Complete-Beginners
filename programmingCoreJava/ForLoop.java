e59ca382936d31ef7ed216bd63618bb09f946aa1
JMatharu <jagdeepmatharu88@gmail.com>
2016-02-10 20:19:15 -0500

    For Loop

---
+++ programmingCoreJava/ForLoop.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package programmingCoreJava;
+
+/**
+ * @author jmatharu Jagdeep Matharu
+ *
+ */
+public class ForLoop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Standard For loop
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Hello!");
+		}
+	
+	}
+
+}
